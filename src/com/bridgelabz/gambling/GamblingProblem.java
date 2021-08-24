740091a5d13f5e6303ec76dee97be599fb238394
namancg <naman.c@perfios.com>
2021-08-24 08:39:20 +0530

    UC1 - bet and stake

---
+++ src/com/bridgelabz/gambling/GamblingProblem.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.gambling;
+import java.util.*;
+public class GamblingProblem {
+
+	public static void main(String[] args) {
+		int stake= 0;
+		int bet = 0;
+		System.out.println("Enter the Stake:");
+		Scanner sc= new Scanner(System.in);
+		stake=sc.nextInt();
+		System.out.println("Enter the Bet:");
+		bet=sc.nextInt();
+	}
+
+}
